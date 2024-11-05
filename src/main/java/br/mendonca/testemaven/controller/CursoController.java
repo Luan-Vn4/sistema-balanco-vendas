2eb79d6779911cd45900fddffdf6fbfafef7873a
holyvieri <beatriz.oliveirac@upe.br>
2024-11-05 16:21:32 -0300

    fix: cursoController bug e userUuid

--- src/main/java/br/mendonca/testemaven/controller/CursoController.java
+++ src/main/java/br/mendonca/testemaven/controller/CursoController.java
@@ -2,18 +2,20 @@ package br.mendonca.testemaven.controller;
 
 import br.mendonca.testemaven.model.entities.Curso;
 import br.mendonca.testemaven.services.CursoService;
+import br.mendonca.testemaven.services.dto.UserDTO;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@WebServlet("/cursos", "/cursos/create", "/cursos/update")
+@WebServlet({"/cursos", "/cursos/create", "/cursos/update"})
 public class CursoController extends HttpServlet {
 
     private static final CursoService cursoService = new CursoService();
@@ -47,6 +49,80 @@ public class CursoController extends HttpServlet {
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getSession(false) == null) {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        if (req.getServletPath().equals("/cursos/create")) {
+            getCreationPage(req, resp);
+        } else if (req.getServletPath().equals("/cursos/update")) {
+            getUpdatePage(req, resp);
+        } else if (req.getParameterMap().isEmpty()) {
+            getAll(req, resp);
+        } else {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private void getCreationPage(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getSession(false) == null) {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        try {
+            req.setAttribute("userUuid", ((UserDTO) req.getSession().getAttribute("user")).getUuid());
+            req.getRequestDispatcher("/cursos/criar-curso.jsp").forward(req, resp);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public void getUpdatePage(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //TODO página de update
+    }
+
+    private void getAll(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            List<Curso> cursos = cursoService.listAllCurso();
+            req.setAttribute("curso", cursos);
+            getServletContext().getRequestDispatcher("/cursos/listar-cursos.jsp").forward(req, resp);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE , e.getMessage(), e);
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        if (req.getSession(false) == null) {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        UserDTO user = (UserDTO) req.getSession().getAttribute("user");
+        System.out.println(user.getName());
+
+        if (!req.getParameterMap().containsKey("uuid")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String uuid = req.getParameter("uuid");
+        try {
+            cursoService.deletar(UUID.fromString(uuid));
+        } catch (Exception e) {
+            logger.log(Level.SEVERE,"UUID" + uuid + "inválido", e);
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 
 
 }

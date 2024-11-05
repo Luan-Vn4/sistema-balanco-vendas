f6c5b5cfa2834f10419c43a678c5beebc3a7cf37
Luan-Vn4 <luanvilaca1@gmail.com>
2024-11-05 16:19:18 -0300

    feat: adicionada página para criar um professor

--- src/main/java/br/mendonca/testemaven/controller/professor/ProfessorController.java
+++ src/main/java/br/mendonca/testemaven/controller/professor/ProfessorController.java
@@ -8,12 +8,41 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("CallToPrintStackTrace")
 @WebServlet(value = {"/professores", "/professores/create"})
 public class ProfessorController extends HttpServlet {
 
     ProfessorService professorService = new ProfessorService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getSession(false) == null) {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        Map<String, String[]> params = req.getParameterMap();
+        if (params.isEmpty() | !params.containsKey("nome") | !params.containsKey("salario")
+            | !params.containsKey("ativo")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Há dados do professor que estão faltando");
+        }
+
+        Professor professor = new Professor();
+        professor.setNome(req.getParameter("nome"));
+        professor.setSalario(Double.parseDouble(req.getParameter("salario")));
+        professor.setAtivo(Boolean.parseBoolean(req.getParameter("ativo")));
+
+        try {
+            professorService.create(professor);
+            resp.sendRedirect(req.getContextPath() + "/professores");
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar professor: " + professor);
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -25,7 +54,7 @@ public class ProfessorController extends HttpServlet {
         if (req.getServletPath().equals("/professores")) {
             getListPage(req, resp);
         } else if (req.getServletPath().equals("/professores/create")) {
-            // TODO servlet para criação do professor
+            getCreationPage(req, resp);
         } else {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
@@ -38,6 +67,16 @@ public class ProfessorController extends HttpServlet {
             req.getRequestDispatcher("/professor/listar-professores.jsp").forward(req, resp);
         } catch (Exception e) {
             System.out.println("Erro ao listar os professores");
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void getCreationPage(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            req.getRequestDispatcher("/professor/criar-professor.jsp").forward(req, resp);
+        } catch (Exception e) {
+            System.out.println("Erro ao obter página de criar professor");
             e.printStackTrace();
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }

f2ff3cd683674f65062898040f1b6ebc8065e2ca
holyvieri <beatriz.oliveirac@upe.br>
2024-11-19 14:41:23 -0300

    feat: atributos adicionados no `user` e `userDTO`

--- src/main/java/br/mendonca/testemaven/services/dto/UserDTO.java
+++ src/main/java/br/mendonca/testemaven/services/dto/UserDTO.java
@@ -9,6 +9,9 @@ public class UserDTO {
 	private UUID uuid;
 	private String name;
 	private String email;
+	private int idade;
+	private boolean status;
+
 	
 	public String getName() {
 		return name;
@@ -31,11 +34,27 @@ public class UserDTO {
 		this.uuid = uuid;
 	}
 
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
 	public static UserDTO userMapper(User user) {
 		UserDTO dto = new UserDTO();
 		dto.setUuid(user.getUuid());
 		dto.setName(user.getName());
 		dto.setEmail(user.getEmail());
+		dto.setIdade(user.getIdade());
+		dto.setStatus(user.isStatus());
 		
 		return dto;
 	}
